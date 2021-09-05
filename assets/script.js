6db8a1f363ebea7aef3f672e61718b7a3b3853b6
Simon Wilder <simonwolfewilder@gmail.com>
2021-09-05 15:13:00 -0700

    first commit, added some initial html and JS

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+const questions = [
+   {
+       question: "Commonly used datatypes DO NOT include:",
+       choices: ["string", "boolean", "alerts", "numbers"],
+       answer: "alerts"
+   },
+
+   {
+       question: "The condition in an if/else statement is enclosed with ____.",
+       choices: ["quotes", "curly braces", "parenthesis", "square brackets"],
+       answer: "parenthesis"
+   },
+
+   {
+       question: "Arrays in javascript can be used to store ___.",
+       choices: ["numbers and strings", "booleans", "other arrays", "all of the above"],
+       answer: "all of the above"
+   },
+
+   {
+       question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+       choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+       answer: "console log"
+   }
+];
+
+
+
